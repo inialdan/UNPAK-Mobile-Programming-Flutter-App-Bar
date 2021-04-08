3ecb95d660064de8ca03d35554150d1bc02f0a53
Aldan Rizki Santosa <projectarizki@gmail.com>
2021-04-08 09:08:24 +0700

    Create project

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            color: Colors.blue,
+            icon: Icon(Icons.view_headline_rounded),
+            onPressed: () {},
+          ),
+          title: Text('Aldan Industries', style: TextStyle(color: Colors.blue)),
+          actions: <Widget>[
+            IconButton(
+              color: Colors.blue,
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              color: Colors.blue,
+              icon: Icon(Icons.account_circle),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image(
+                  image: AssetImage('assets/images/memoji_aldan.jpg'),
+                ),
+              ),
+              Text('Aldan Rizki Santosa',
+                  style: TextStyle(color: Colors.blue, fontSize: 30)),
+              Text('065118112',
+                  style: TextStyle(color: Colors.blue, fontSize: 25)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
